0ead490b71e215faab1aa9dda9947ef7d505470e
Luke Andrew <luke.bzr@la.id.au>
2011-04-16 22:04:20 +0930

    write a wav file instead of raw data

--- test/whysynth_voice_render_test.c
+++ test/whysynth_voice_render_test.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <unistd.h>
 
 #include "src/whysynth_voice_render.c"
 
 #define SEGMENT_LENGTH 2750
+#define FREQ_STEPS 10
+#define RES_STEPS 20
+#define DATA_SIZE (SEGMENT_LENGTH * FREQ_STEPS * RES_STEPS * 2)
+#define WAV_HEADER  { \
+0x57, 0x41, 0x56, 0x45, 0x66, 0x6d, 0x74, 0x20,  0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, \
+0x44, 0xac, 0x00, 0x00, 0x88, 0x58, 0x01, 0x00,  0x02, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61 \
+}
+#define WAV_HEADER_SIZE 32
+#define WAV_FILESIZE (10 + WAV_HEADER_SIZE + DATA_SIZE)
 
 void test_filter(float freq, float key_mod, float res, FILE *output)
 {
@@ -42,11 +52,10 @@ void test_filter(float freq, float key_mod, float res, FILE *output)
     for (i = 0; i < SEGMENT_LENGTH; i++)
         audio[i] = (int16_t)roundf(out[i] * 30000 );
 
-    // play this output with: play -r 44100 -ts16 output
     fwrite(audio, 2, SEGMENT_LENGTH, output);
 
 
-    // check return values
+    // TODO check return values
 }
 
 int main(void) 
@@ -55,13 +64,23 @@ int main(void)
     // svcf->frequency: ~0.001-0.05 from note value (middle C ~ 0.0055)
     // res: 0-1
 
-    FILE *output = fopen("output","wb");
+    FILE *output = fopen("filter_sweep_test.wav","wb");
 
-    float i,r;
+    int riff_size = WAV_FILESIZE - 8;
+    fwrite("RIFF", 1, 4, output);
+    fwrite(&riff_size, 4, 1, output);
 
-    for (i=0; i<10; i++)
-        for (r=0; r<1; r += 0.05)
-            test_filter(i, 0.0055, r, output);
+    uint8_t header[] = WAV_HEADER;
+    fwrite(&header, 1, 32, output);
+
+    uint32_t data_size = DATA_SIZE;
+    fwrite(&data_size, 4, 1, output);
+
+    float freq,r;
+
+    for (freq=0; freq < FREQ_STEPS; freq++)
+        for (r=0; r < 1; r += (1.0/RES_STEPS))
+            test_filter(freq, 0.0055, r, output);
 
     fclose(output);
 
