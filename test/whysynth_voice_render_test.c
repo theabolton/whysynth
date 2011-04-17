389985f1dc4c4c349574fcca4a4ebb51c1566d55
Luke Andrew <luke.bzr@la.id.au>
2011-04-17 17:41:36 +0930

    add long and short segment length options for testing filters by ear or for comparing against smaller, known-good files

--- test/whysynth_voice_render_test.c
+++ test/whysynth_voice_render_test.c
@@ -6,12 +6,22 @@
 
 #include "src/whysynth_voice_render.c"
 
-#define SEGMENT_LENGTH 2750
+// set CHECK_OUTPUT to true when you want to use the LONG_SEGMENT_LENGTH to
+// check the filter output by ear
+#define CHECK_OUTPUT
+#define LONG_SEGMENT_LENGTH 2750
+#define SHORT_SEGMENT_LENGTH 64
+
 #define FREQ_STEPS 10
 #define RES_STEPS 20
 
 // These defines give us a header for a 44.1kHz 16 bit mono WAV file
-#define DATA_SIZE (SEGMENT_LENGTH * FREQ_STEPS * RES_STEPS * 2)
+#ifdef CHECK_OUTPUT
+  #define DATA_SIZE (LONG_SEGMENT_LENGTH * FREQ_STEPS * RES_STEPS * 2)
+#else
+  #define DATA_SIZE (SHORT_SEGMENT_LENGTH * FREQ_STEPS * RES_STEPS * 2)
+#endif
+
 #define WAV_HEADER  { \
 0x57, 0x41, 0x56, 0x45, 0x66, 0x6d, 0x74, 0x20,  0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, \
 0x44, 0xac, 0x00, 0x00, 0x88, 0x58, 0x01, 0x00,  0x02, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61 \
@@ -19,7 +29,7 @@
 #define WAV_HEADER_SIZE 32
 #define WAV_FILESIZE (10 + WAV_HEADER_SIZE + DATA_SIZE)
 
-void test_filter(float freq, float key_mod, float res, FILE *output)
+void test_filter(uint32_t length, float freq, float key_mod, float res, FILE *output)
 {
     int i;
     float fzero = 0.0f;
@@ -37,24 +47,27 @@ void test_filter(float freq, float key_mod, float res, FILE *output)
     param->freq_mod_amt = &fzero;
     param->mparam = &fzero;
 
-    float in[SEGMENT_LENGTH]; 
+    float in[length]; 
+    float out[length];
 
     // TODO only call this for automated tests to prevent audible patterns forming
     // when verifying the output by ear
     //srand(0);
 
-    for (i = 0; i<SEGMENT_LENGTH; i++)
+    for (i = 0; i < length; i++)
+    {
         in[i] = ((float)random() / (float)RAND_MAX) - 0.5f;
+        out[i] = 0;
+    }
 
-    float out[SEGMENT_LENGTH] = {0};
 
-    vcf_2pole(SEGMENT_LENGTH, param, voice, &vcf_state, freq, (float *)&in, (float *)&out);
+    vcf_2pole(length, param, voice, &vcf_state, freq, (float *)&in, (float *)&out);
 
-    int16_t audio[SEGMENT_LENGTH];
-    for (i = 0; i < SEGMENT_LENGTH; i++)
+    int16_t audio[length];
+    for (i = 0; i < length; i++)
         audio[i] = (int16_t)roundf(out[i] * 25000 );
 
-    fwrite(audio, 2, SEGMENT_LENGTH, output);
+    fwrite(audio, 2, length, output);
 
 
     // TODO check return values
@@ -87,9 +100,15 @@ int main(void)
 
     float freq,r;
 
+#ifdef CHECK_OUTPUT
+    const uint32_t test_segment_length = LONG_SEGMENT_LENGTH;
+#else
+    const uint32_t test_segment_length = SHORT_SEGMENT_LENGTH;
+#endif
+
     for (r=0; r < 0.9; r += (1.0/RES_STEPS))
         for (freq=0; freq < FREQ_STEPS; freq++)
-            test_filter(freq, 0.0055, r, output);
+            test_filter(test_segment_length, freq, 0.0055, r, output);
 
     fclose(output);
 
