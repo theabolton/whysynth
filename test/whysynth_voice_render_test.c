18536b5519b17433a3b971227601f77426521f75
Luke Andrew <luke.bzr@la.id.au>
2011-04-19 19:09:27 +0930

    some refactoring to generate sweeps for each filter

--- test/whysynth_voice_render_test.c
+++ test/whysynth_voice_render_test.c
@@ -8,27 +8,26 @@
 
 // change SEGMENT_LENGTH to a large vaule (3000+) when you want to 
 // check the filter output by ear
-#define SEGMENT_LENGTH 3000
+#define SEGMENT_LENGTH 2000
 
-#define FREQ_STEPS 10
-#define RES_STEPS 20
+#define SWEEP_STEPS 10
 
 // These defines give us a header for a 44.1kHz 16 bit mono WAV file
-#define DATA_SIZE (SEGMENT_LENGTH * FREQ_STEPS * RES_STEPS * 2)
 
 #define WAV_HEADER  { \
   0x57, 0x41, 0x56, 0x45, 0x66, 0x6d, 0x74, 0x20,  0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, \
   0x44, 0xac, 0x00, 0x00, 0x88, 0x58, 0x01, 0x00,  0x02, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61 \
 }
 #define WAV_HEADER_SIZE 32
-#define WAV_FILESIZE (10 + WAV_HEADER_SIZE + DATA_SIZE)
+
+typedef void (*filter_func_t)(unsigned long, y_svcf_t *, y_voice_t *, struct vvcf *, float , float *, float *);
 
 // Parameter ranges:
 //
 // freq: 0-50 from GUI
 // key_mod: ~0.001-0.05 from note value (middle C ~ 0.0055)
 // res: 0-1
-void test_filter(uint32_t length, float freq, float key_mod, float res, FILE *output)
+void generate_segment(filter_func_t filter, float freq, float key_mod, float drive, float res, FILE *output)
 {
     int i;
     float fzero = 0.0f;
@@ -46,60 +45,83 @@ void test_filter(uint32_t length, float freq, float key_mod, float res, FILE *ou
     param->source = &fzero;
     param->freq_mod_src = &fzero;
     param->freq_mod_amt = &fzero;
-    param->mparam = &fzero;
+    param->mparam = &drive;
 
-    float in[length], out[length];
+    float in[SEGMENT_LENGTH], out[SEGMENT_LENGTH];
 
-    // TODO only call this for automated tests to prevent audible patterns forming
-    // when verifying the output by ear
-    //srand(0);
-
-    for (i = 0; i < length; i++)
+    for (i = 0; i < SEGMENT_LENGTH; i++)
     {
         in[i] = ((float)random() / (float)RAND_MAX) - 0.5f;
         out[i] = 0;
     }
 
-    vcf_2pole(length, param, voice, &vcf_state, freq, (float *)&in, (float *)&out);
+    filter(SEGMENT_LENGTH, param, voice, &vcf_state, freq, (float *)&in, (float *)&out);
 
-    int16_t audio[length];
-    for (i = 0; i < length; i++)
-        audio[i] = (int16_t)roundf(out[i] * 25000 );
+    int16_t audio[SEGMENT_LENGTH];
 
-    assert(fwrite(audio, 2, length, output) == length);
+    for (i = 0; i < SEGMENT_LENGTH; i++)
+        audio[i] = (int16_t)roundf(out[i] * 17000 );
+
+    assert(fwrite(audio, 2, SEGMENT_LENGTH, output) == SEGMENT_LENGTH);
 }
 
-FILE *create_wav(char *filename)
+FILE *create_wav(uint32_t data_size, char *filename)
 {
     FILE *output = fopen(filename,"wb");
 
-    int riff_size = WAV_FILESIZE - 8;
+    int riff_size = WAV_HEADER_SIZE + data_size + 2;
     fwrite("RIFF", 1, 4, output);
     fwrite(&riff_size, 4, 1, output);
 
     uint8_t header[] = WAV_HEADER;
     fwrite(&header, 1, 32, output);
 
-    uint32_t data_size = DATA_SIZE;
     fwrite(&data_size, 4, 1, output);
 
     return output;
 }
 
-int main(void) 
+static void sweep(filter_func_t filter, float drive, FILE *output)
 {
+    float freq, r;
 
-    FILE *output = create_wav("filter_sweep_test.wav");
+    for (r=0; r < 0.9; r += (1.0/SWEEP_STEPS))
+        for (freq=0; freq < SWEEP_STEPS; freq++)
+            generate_segment(filter, freq, 0.0055, drive, r, output);
+}
 
-    float freq,r;
+void sweep_filter(filter_func_t filter, bool sweep_drive, char *filename)
+{
+    srand(0);
 
-    const uint32_t test_segment_length = SEGMENT_LENGTH;
+    uint32_t data_size = SEGMENT_LENGTH * SWEEP_STEPS * SWEEP_STEPS * 2;
 
-    for (r=0; r < 0.9; r += (1.0/RES_STEPS))
-        for (freq=0; freq < FREQ_STEPS; freq++)
-            test_filter(test_segment_length, freq, 0.0055, r, output);
+    if (sweep_drive)
+        data_size *= SWEEP_STEPS;
+
+    FILE *output = create_wav(data_size, filename);
+
+    float drive = 0;
+
+    // generate test segments by sweeping the frequency, resonance and optionally drive.
+    if (sweep_drive)
+        for (drive=0; drive < 1.0; drive += (1.0/SWEEP_STEPS))
+            sweep(filter, drive, output);
+    else
+        sweep(filter, drive, output);
+
 
     fclose(output);
+}
+
+int main(void) 
+{
+    sweep_filter(&vcf_2pole,     false, "vcf_2pole_sweep.wav");
+    sweep_filter(&vcf_4pole,     false, "vcf_4pole_sweep.wav");
+    sweep_filter(&vcf_mvclpf,    false, "vcf_mvclpf_sweep.wav");
+    sweep_filter(&vcf_clip4pole, true,  "vcf_clip4pole_sweep.wav");
+    sweep_filter(&vcf_bandpass,  false, "vcf_bandpass_sweep.wav");
+    sweep_filter(&vcf_amsynth,   false, "vcf_amsynth_sweep.wav");
 
     return 0;
 }
